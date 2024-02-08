d44e61518f8de76347d95e156b10191ee04b4161
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-02-08 18:14:38 -0300

    feat: Add moment validation and full date prompt

--- s05a03/scripts.js
+++ s05a03/scripts.js
@@ -1,11 +1,22 @@
-let year = Number(prompt("Qual é o ano do seu nascimento?"));
-let month = Number(prompt("Qual é o mês do seu nascimento?")) - 1;
-let day = Number(prompt("Qual é o dia do seu nascimento?"));
+// let year = prompt("Qual é o ano do seu nascimento?");
+// let month = prompt("Qual é o mês do seu nascimento?");
+// let day = rompt("Qual é o dia do seu nascimento?");
 
-function parseAge(year, month, day){
+let birthDate = prompt("Qual é data do seu nascimento? (FORMATO YYYY/MM/DD)")
+let splitDate = birthDate.split("/");
+let year = splitDate[0];
+let month = splitDate[1];
+let day = splitDate[2];
+
+function calculateAge(year, month, day){
+    console.log(`${year}/${month}/${day}`);
+    if (!moment(`${year}/${month}/${day}`, 'YYYY/MM/DD',true).isValid()) {
+        alert("A data inserida não é válida.");
+        return 0
+    }
     let today = new Date();
     //console.log(today);
-    let birthDate = new Date(year, month, day);
+    let birthDate = new Date(Number(year), Number(month) - 1, Number(day));
     //console.log(birthDate);
     let age = today.getFullYear() - birthDate.getFullYear();
     //console.log(age);
@@ -17,4 +28,5 @@ function parseAge(year, month, day){
     return age;
 }
 
-alert("Você tem " + parseAge(year, month, day) + " anos.");
+let age = calculateAge(year, month, day);
+age ? alert("Você tem " + age + " anos.") : alert("Não foi possível calcular sua idade.");

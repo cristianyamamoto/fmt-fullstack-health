541875d0246c0dc49f57b64dfaecd11a5971354a
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-02-08 21:05:23 -0300

    docs: Add comments throughout the code for exercises

--- s04a05/scripts/script.js
+++ s04a05/scripts/script.js
@@ -1,4 +1,4 @@
-// DECLARAR VÁRIAVEIS DO BOARD 
+// EXERCÍCIO 1 - DECLARAÇÃO DE VARIÁVEIS
 let box = 32;
 let board = document.getElementById("snake");
 let context = board.getContext("2d");
@@ -17,11 +17,13 @@ let food = {
     y: Math.floor(Math.random() * 15 + 1) * box
 };
 
+// EXERCÍCIO 2 - CRIAÇÃO DO BACKGROUD
 function createBG() {
     context.fillStyle = "black";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
+// EXERCÍCIO 3 - CRIAÇÃO DA COBRA
 function createSnake() {
     for (let i in snake) {
         context.fillStyle = "gold";
@@ -29,12 +31,13 @@ function createSnake() {
       }
 }
 
+// EXERCÍCIO 4 - CRIAÇÃO DA COMIDA
 function createFood() {
     context.fillStyle = "silver";
     context.fillRect(food.x, food.y, box, box);
 }
 
-// passo 5 função update() no step by step
+// EXERCÍCIO 5 - MECÂNICA DO MOVIMENTO - função update() no passo a passo
 function moveSnake(event) {
     if (event.keyCode == 37 && direction != 'right') direction = 'left';
     if (event.keyCode == 38 && direction != 'down') direction = 'up';
@@ -42,7 +45,14 @@ function moveSnake(event) {
     if (event.keyCode == 40 && direction != 'up') direction = 'down';
 }
 
+// EXERCÍCIO 6 - ALGORITMO PRINCIPAL P1
 function startGame(){
+    // ESSA PARTE COMENTADA SUBSTITUI A FUNÇÃO DO setInterval() COM requestAnimationFrame()
+    // let fps = 25;
+    // setTimeout(() => {
+    //     requestAnimationFrame(startGame);
+    // }, 1000 / fps);
+
     if (snake[0].x > 15 * box && direction == "right") snake[0].x = 0;
     if (snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
@@ -52,6 +62,7 @@ function startGame(){
         if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
             clearInterval(game);
             alert("Game Over");
+            // return;
         }
     }
 
@@ -59,6 +70,7 @@ function startGame(){
     createSnake();
     createFood();
 
+    // EXERCÍCIO 7 - ALGORITMO PRINCIPAL P2
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
@@ -67,6 +79,7 @@ function startGame(){
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
+    // EXERCÍCIO 8 - ALGORITMO PRINCIPAL P3
     if (snakeX != food.x || snakeY != food.y) {
         snake.pop(); 
     } else {
@@ -82,5 +95,12 @@ function startGame(){
     snake.unshift(newHead);
 }
 
+// ESSA PARTE COMENTADA SUBSTITUI A FUNÇÃO DO setInterval() COM requestAnimationFrame()
+// window.onload = function() {
+//     requestAnimationFrame(startGame);
+//     document.addEventListener("keydown", moveSnake);
+// }
+
+// EXERCÍCIO 9 - COMEÇANDO O JOGO
 document.addEventListener("keydown", moveSnake);
 let game = setInterval(startGame, 100);

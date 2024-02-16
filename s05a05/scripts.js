8020c109d9394ba959f0b6f174150c7ec6749a96
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-02-16 16:47:52 -0300

    feat: Develop to do list solution

---
+++ s05a05/scripts.js
@@ -0,0 +1,44 @@
+let botaoAdicionar = document.querySelector("#botaoAdicionar");
+botaoAdicionar.addEventListener("click", adicionarTarefa);
+
+function adicionarTarefa() {
+    let tarefa = document.querySelector("#tarefa").value;
+    let listaDeAfazeres = document.querySelector("#to-do-list ul");
+
+    console.log(listaDeAfazeres.querySelectorAll("li").length);
+
+    let listItem = document.createElement("li");
+    listItem.id = "tarefa-" + (listaDeAfazeres.querySelectorAll("li").length + 1);
+    listItem.innerHTML = 
+    `<div class="flex-box">
+        <div>
+            <input type="checkbox" class="caixaSelecaoTarefa">
+            <span>${tarefa}</span>
+        </div>
+        <button class="delete-button"><i class='bx bxs-trash'></i></button>
+    </div>
+    `;
+    listaDeAfazeres.appendChild(listItem);
+    // console.log(listaDeAfazeres);
+
+    let caixa = listItem.querySelector(".caixaSelecaoTarefa")
+    caixa.addEventListener('change', function(){
+        this.parentNode.classList.toggle('checked');
+    });
+    
+    let botaoDeletar = listItem.querySelector('.delete-button');
+    botaoDeletar.addEventListener('click', function(){
+        this.closest("li").remove();
+    });
+}
+
+let caixas = document.querySelectorAll('.caixaSelecaoTarefa');
+// Transforma os elementos do HTMLCollection em array e para cada elemento atribui um evento para mundança de estado
+Array.from(caixas).forEach(caixa => caixa.addEventListener('change', function(){
+    this.parentNode.classList.toggle('checked');
+}));
+
+let botoesDeletar = document.querySelectorAll('.delete-button');
+Array.from(botoesDeletar).forEach(botao => botao.addEventListener('click', function(){
+    this.closest("li").remove();
+}));

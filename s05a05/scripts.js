299d738ac97e543b32ebb0eb4a38c69a6a6f0e91
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-02-22 16:32:45 -0300

    add same chore name validity

--- s05a05/scripts.js
+++ s05a05/scripts.js
@@ -10,40 +10,50 @@ contadorTarefas.innerHTML = document.querySelectorAll("#to-do-list ul li").lengt
 function adicionarTarefa() {
     if(tarefa.value){
         let listaDeAfazeres = document.querySelector("#to-do-list ul");
-        let listItem = document.createElement("li");
-        listItem.id = "tarefa-" + (listaDeAfazeres.querySelectorAll("li").length + 1);
-        listItem.innerHTML = 
-        `<div class="flex-box">
-            <div>
-                <input type="checkbox" class="caixaSelecaoTarefa">
-                <span>${tarefa.value}</span>
+
+        if (!Array.from(listaDeAfazeres.querySelectorAll("li"))
+                .find(t => t.querySelector("span").innerText == tarefa.value)) {
+
+            let listItem = document.createElement("li");
+            listItem.id = "tarefa-" + (listaDeAfazeres.querySelectorAll("li").length + 1);
+            listItem.innerHTML = 
+            `<div class="flex-box">
+                <div>
+                    <input type="checkbox" class="caixaSelecaoTarefa">
+                    <span>${tarefa.value}</span>
+                </div>
+                <button class="delete-button"><i class='bx bxs-trash'></i></button>
             </div>
-            <button class="delete-button"><i class='bx bxs-trash'></i></button>
-        </div>
-        `;
-        listaDeAfazeres.appendChild(listItem);
+            `;
+            listaDeAfazeres.appendChild(listItem);
 
-        let caixa = listItem.querySelector(".caixaSelecaoTarefa")
-        caixa.addEventListener('change', function(){
-            this.parentNode.classList.toggle('checked');
-        });
-        // caixa.onchange = function() {
-        //     caixa.parentNode.classList.toggle('checked');
-        // };
-        
-        let botaoDeletar = listItem.querySelector('.delete-button');
-        botaoDeletar.addEventListener('click', function(){
-            if (confirm("A tarefa será excluída permanentemente!")) {
-                this.closest("li").remove();
-                contadorTarefas.innerHTML = Number(contadorTarefas.innerHTML) - 1;
-                // contadorTarefas.innerHTML = document.querySelectorAll("#to-do-list ul li").length;
-            } 
-        });
+            let caixa = listItem.querySelector(".caixaSelecaoTarefa")
+            caixa.addEventListener('change', function(){
+                this.parentNode.classList.toggle('checked');
+            });
+            // caixa.onchange = function() {
+            //     caixa.parentNode.classList.toggle('checked');
+            // };
+            
+            let botaoDeletar = listItem.querySelector('.delete-button');
+            botaoDeletar.addEventListener('click', function(){
+                if (confirm("A tarefa será excluída permanentemente!")) {
+                    this.closest("li").remove();
+                    contadorTarefas.innerHTML = Number(contadorTarefas.innerHTML) - 1;
+                    // contadorTarefas.innerHTML = document.querySelectorAll("#to-do-list ul li").length;
+                } 
+            });
 
-        tarefa.value = "";
-        contadorTarefas.innerHTML = Number(contadorTarefas.innerHTML) + 1;
-        // contadorTarefas.innerHTML = document.querySelectorAll("#to-do-list ul li").length;
+            tarefa.value = "";
+            contadorTarefas.innerHTML = Number(contadorTarefas.innerHTML) + 1;
+            // contadorTarefas.innerHTML = document.querySelectorAll("#to-do-list ul li").length;
+        }
+        else {
+            tarefa.setCustomValidity("Tarefa já existe!");
+            tarefa.reportValidity();
+        }
     } else {
+        tarefa.setCustomValidity("Preencha o campo da tarefa!");
         tarefa.reportValidity();
     }
     tarefa.focus();

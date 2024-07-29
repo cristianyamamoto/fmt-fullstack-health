26aa0b55b670fc4a0e83dfccfd0022bdce7b78ee
Cristian Yamamoto <yamamotocristian@gmail.com>
2024-07-29 06:15:58 -0300

    create project and add application.properties

---
+++ m02/s10/doctor-registration/src/main/java/br/senai/lab365/doctor_registration/DoctorRegistrationApplication.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.doctor_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorRegistrationApplication.class, args);
+	}
+
+}
---
+++ m02/s10/doctor-registration/src/test/java/br/senai/lab365/doctor_registration/DoctorRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package br.senai.lab365.doctor_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
